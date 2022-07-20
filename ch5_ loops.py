8cd0a2706b39e1b70f7e4a7452c68ec653c810f9
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-20 09:25:34 -0500

    ch5 loops and calculator file

---
+++ ch5_ loops.py
@@ -0,0 +1,20 @@
+fruits = ["apple", "orange", "pears", "cherries", "grapes"]
+
+
+# Usually typed out
+"""
+for i in fruits:
+    print(f"Would you like {i}?")
+"""
+
+
+# Easier to read for new coders
+for fruit in fruits:
+    print(f"Would you like {fruit}?")
+
+
+# Prints numbers from 1 - 10 
+for number in range(1,11):
+    print(f"Number {number}.")
+    
+    
