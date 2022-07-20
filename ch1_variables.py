dfa6b5ce4d00d750f877a35884e31e29b479dadb
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-20 08:53:37 -0500

    ch1 variables

---
+++ ch1_variables.py
@@ -0,0 +1,42 @@
+
+# python print() statements
+
+_cars = 23
+cars = 32
+
+CARS = 43
+
+number_of_cars = 3
+kind_of_cars = 'Ferrari'
+
+print(cars)
+print(CARS)
+print(kind_of_cars)
+
+"""
+This is a multi-line comment
+You can use this kind of comment to make longer notes
+as you are learning.
+In python, these are often used as docstrings.
+
+We will do formatting now.
+"""
+
+name = "Janelle Jones"
+type_of_car = "Rolls Royce"
+school = "Foundation Elementary School"
+greeting = 'hello'
+print(greeting)
+
+print(name + school)
+print(name + " " + school)
+print(name + " works at " + school +".")
+
+#python string.format()
+print("{} works at {}.".format(name, school))
+print(f"{name} works at {school}.")
+
+'''
+multi line comment
+multi line comment
+'''
