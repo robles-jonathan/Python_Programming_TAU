8b9e5a22d0e0f181c2e1288a30ef03f8494bfe9f
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-20 08:56:38 -0500

    ch2 functions

---
+++ ch2_functions.py
@@ -0,0 +1,24 @@
+
+
+def addition():
+    a = 10
+    b = 30
+    print(a + b)
+
+
+addition()
+
+#Geting user input
+# input("what is your name?")
+
+
+#Python standard is to put 2 lines between the end of your function and the beginning of your next one.
+
+
+def addition_of_inputs():
+    a = int(input("Enter a number. "))
+    b = int(input("Please enter another number. "))
+    print(a + b)
+    
+    
+addition_of_inputs()
